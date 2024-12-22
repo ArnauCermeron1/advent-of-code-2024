781dd6442154fd53ef945c9e088182bbba253d7f
ArnauCermeron <arnaucermeron@gmail.com>
2024-12-22 22:44:18 +0100

    implement solution for day 01 with input processing and calculations for distance and similarity

--- day01/solution.js
+++ day01/solution.js
@@ -1,7 +1,45 @@
+// Part 1
 const readInput = require('../utils/readInput');
+const lines = readInput('input.txt');
 
-const lines = readInput('sample.txt');
-console.log(lines);
+// Split the input into two arrays
+const first = lines.map(lines=>parseInt(lines.split(/\s+/)[0], 10));
+const second = lines.map(lines=>parseInt(lines.split(/\s+/)[1], 10));
+let sum = 0;
+
+// Order the list
+first.sort((a,b)=>a-b);
+second.sort((a,b)=>a-b);
+
+// Iterate through the list and calculate the final distance
+for(let i = 0; i < first.length; i++){
+    let num1 = first[i];
+    let num2 = second[i];
+    let difference = Math.abs(num1-num2);
+    sum += difference;
+}
+
+console.log(sum);
+
+// Part 2
+
+let similarity = 0;
+let count = 0;
+
+// Iterate through the list and calculate the similarity
+for (let j = 0; j < first.length; j++){
+    let key = first[j];
+    for (let k = 0; k < second.length; k++){
+        if (key === second[k]){
+            count++;
+        }
+    }
+    similarity = similarity + (count * key);
+    count = 0;
+}
+
+console.log(similarity);
+
 
 
 
